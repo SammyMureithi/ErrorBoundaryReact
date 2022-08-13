db8cceb888e3976385192528fcc49bfc700e0820
Sammy Mureithi <sammymureithi20@gmail.com>
2022-08-14 00:04:57 +0300

    Error Boundary

---
+++ error-boundary/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Hero from './Components/Hero';
+import ErrorBoundary from './Components/ErrorBoundary';
+
+function App() {
+  return (
+    <div className="App">
+      <ErrorBoundary>
+      <Hero heroName="Mure" />
+      </ErrorBoundary>
+     
+      <ErrorBoundary>
+      <Hero heroName="Joker" />
+      </ErrorBoundary>
+      
+    </div>
+  );
+}
+
+export default App;
